0b5b3b8a2a4b5e4e4fd9b510f9a1dd5cbdf7cded
Cypherspot <awolesiboluwatife@gmail.com>
2022-02-09 20:17:31 +0100

    application to app

--- payment/wsgi.py
+++ payment/wsgi.py
@@ -5,4 +5,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'payment.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+app = get_wsgi_application()
+
